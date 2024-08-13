568163e471e53629d0791a4477fcc80dd74383ee
Kunal Phund <kunalphundkp736@gmail.com>
2024-08-14 01:00:58 +0530

    Modified some things in the project

---
+++ LuxeStay/LuxeStay-backend/src/test/java/com/luxestay/LuxeStay/LuxeStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxestay.LuxeStay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxeStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ LuxeStay/luxestay-frontend/src/component/common/Footer.jsx
@@ -0,0 +1,12 @@
+const FooterComponent = () => {
+
+    return (
+        <footer>
+            <span className="my-footer">
+                LuxeStay | All Right Reserved &copy; {new Date().getFullYear()}
+            </span>
+        </footer>
+    );
+};
+
+export default FooterComponent; 
